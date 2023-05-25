056975cf12b4d5f57c3c3e358db189fc92a6f965
Abdullah All Noman <MD Noman Hossain>
2023-05-25 15:23:37 +0600

    initials commit

--- components/Home/Cetagory/CategoryCart.jsx
+++ components/Home/Cetagory/CategoryCart.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const CategoryCart = () => {
     return (
-        <div className='flex flex-col items-center justify-center bg-white shadow-2xl h-40 w-44 rounded-md m-2'>
+        <div className='flex flex-col items-center justify-center bg-white shadow-xl h-36 w-40 rounded-md m-2'>
             <picture>
-                <img className='w-40 h-36' src="/assets/cat/cat2.png" alt="" />
+                <img className='w-32 h-24' src="/assets/cat/cat2.png" alt="" />
             </picture>
-            <h5 className='font-poppins font-semibold text-lg'>Mackup</h5>
+            <h5 className='font-poppins font-semibold text-lg mb-2'>Mackup</h5>
         </div>
     )
 }
