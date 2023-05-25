fcf2baef3bd1cb79c1973a378b99ece4a2ef9365
Abdullah All Noman <MD Noman Hossain>
2023-05-25 21:28:19 +0600

    initials commit

--- components/Home/NavbarItem/Middle.jsx
+++ components/Home/NavbarItem/Middle.jsx
@@ -6,7 +6,7 @@ import { AiOutlineSearch } from "react-icons/ai"
 
 const Middle = () => {
   return (
-    <div className='container mx-auto flex items-center justify-between gap-2 md:gap-0 py-2 px-3 md:px-0 '>
+    <div className='container mx-auto flex items-center justify-between gap-2 md:gap-0 py-3 md:py-2 px-3 md:px-0 '>
       <div>
         <Image
           alt='logo'
