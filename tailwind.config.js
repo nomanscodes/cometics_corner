24ab9d9de4af8268da17a84e0ab81cd842939539
Abdullah All Noman <MD Noman Hossain>
2023-05-25 21:26:38 +0600

    initials commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,7 @@ module.exports = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       colors: {
-        bgColor: "#f7771d",
+        bgColor: "#41583d",
         gold: "#dfc441",
       },
     },
