75847edfdb1c1db8a115614c4a6c45e0d2fed6ba
Abdullah All Noman <MD Noman Hossain>
2023-05-26 19:51:20 +0600

    initials commit

--- components/Home/Products/TavSection.jsx
+++ components/Home/Products/TavSection.jsx
@@ -7,7 +7,7 @@ const TabSection = () => {
             <h5 className="flex  items-center justify-center text-2xl font-poppins font-bold">
                 Popular Departments
             </h5>
-            <div className="flex items-center justify-center gap-4 mt-3">
+            <div className="flex items-center justify-center gap-4 mt-3 overflow-x-auto scroll-smooth">
                 <button className=" border border-bgColor p-1 md:p-0 md:py-[2px] md:px-[8px] font-poppins text-sm md:text-base md:font-bold text-bgColor rounded">New Arrivals</button>
                 <button className=" border border-bgColor p-1 md:p-0 md:py-[2px] md:px-[8px] font-poppins text-sm md:text-base md:font-bold text-bgColor rounded">Best Seller</button>
                 <button className=" border border-bgColor p-1 md:p-0 md:py-[2px] md:px-[8px] font-poppins text-sm md:text-base md:font-bold text-bgColor rounded">Most Popular</button>
