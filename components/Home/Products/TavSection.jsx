9cca832e321933b67da2be8c2d4d41fb779fe8f7
Abdullah All Noman <MD Noman Hossain>
2023-05-26 19:48:05 +0600

    initials commit

--- components/Home/Products/TavSection.jsx
+++ components/Home/Products/TavSection.jsx
@@ -3,15 +3,15 @@ import ProductCart from "./ProductCart";
 
 const TabSection = () => {
     return (
-        <div className="px-2 md:px-6 mt-4">
+        <div className="px-2 md:px-6 mt-2">
             <h5 className="flex  items-center justify-center text-2xl font-poppins font-bold">
                 Popular Departments
             </h5>
             <div className="flex items-center justify-center gap-4 mt-3">
-                <button className=" border border-bgColor py-[2px] px-[8px] font-poppins text-base font-bold text-bgColor rounded">New Arrivals</button>
-                <button className=" border border-bgColor py-[2px] px-[8px] font-poppins text-base font-bold text-bgColor rounded">Best Seller</button>
-                <button className=" border border-bgColor py-[2px] px-[8px] font-poppins text-base font-bold text-bgColor rounded">Most Popular</button>
-                <button className=" border border-bgColor py-[2px] px-[8px] font-poppins text-base font-bold text-bgColor rounded">Featured</button>
+                <button className=" border border-bgColor p-1 md:p-0 md:py-[2px] md:px-[8px] font-poppins text-sm md:text-base md:font-bold text-bgColor rounded">New Arrivals</button>
+                <button className=" border border-bgColor p-1 md:p-0 md:py-[2px] md:px-[8px] font-poppins text-sm md:text-base md:font-bold text-bgColor rounded">Best Seller</button>
+                <button className=" border border-bgColor p-1 md:p-0 md:py-[2px] md:px-[8px] font-poppins text-sm md:text-base md:font-bold text-bgColor rounded">Most Popular</button>
+                <button className=" border border-bgColor p-1 md:p-0 md:py-[2px] md:px-[8px] font-poppins text-sm md:text-base md:font-bold text-bgColor rounded">Featured</button>
             </div>
             <div className="grid grid-cols-5 gap-3 mt-4">
                 <ProductCart />
