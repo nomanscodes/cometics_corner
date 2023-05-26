56d8d88cd3ad1e1d5501fb231fa734a64f219cd4
Abdullah All Noman <MD Noman Hossain>
2023-05-26 20:13:29 +0600

    initials commit

--- components/Home/Products/TabSection.jsx
+++ components/Home/Products/TabSection.jsx
@@ -3,10 +3,8 @@ import ProductCart from "./ProductCart";
 
 const TabSection = () => {
     return (
-        <div className="px-2 md:px-6">
-            <h5 className="flex  items-center justify-center text-2xl font-poppins font-bold">
-                Popular Departments
-            </h5>
+        <div className="px-2 md:px-6 mt-2">
+            <h1 className='font-semibold text-lg md:text-2xl opacity-90'>Popular Departments</h1>
             <div className="CatCarousel flex items-center justify-start overflow-x-auto scroll-smooth mt-4 gap-3">
                 <div>
                     <button className=" border border-bgColor px-[4px] py-[2px] md:px-[8px] font-poppins text-sm md:text-base md:font-bold text-bgColor rounded whitespace-nowrap">New Arrivals</button>
