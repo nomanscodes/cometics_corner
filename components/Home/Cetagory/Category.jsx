3df2afedb2758e99eb4d895cd8a9ee75a879388e
Abdullah All Noman <MD Noman Hossain>
2023-05-28 23:52:03 +0600

    initials commit

--- components/Home/Cetagory/Category.jsx
+++ components/Home/Cetagory/Category.jsx
@@ -14,16 +14,16 @@ const Category = () => {
   }
 
   return (
-    <div className='px-2 md:px-6 relative md:mt-4 font-poppins '>
-      <div className='hidden md:flex items-center justify-between '>
+    <div className='px-2 md:px-6 relative mt-2 md:mt-4 font-poppins '>
+      <div className='flex items-center justify-between '>
         <div className='flex items-center gap-7'>
-          <h1 className='font-semibold text-lg md:text-2xl opacity-90'>SHOP BY CETEGORY</h1>
+          <h1 className='font-semibold text-sm md:text-2xl opacity-90'>SHOP BY CETEGORY</h1>
           <Link href={""} className='hidden md:flex items-center gap-1 group  '>
             <h1 className='font-medium group-hover:text-bgColor duration-300'>All Category</h1>
             <MdKeyboardArrowRight className='group-hover:text-bgColor duration-300' size={22} />
           </Link>
         </div>
-        <div className='flex items-center gap-2 text-white'>
+        <div className='hidden md:flex items-center gap-2 text-white'>
           <button onClick={scrollLeft} className='bg-bgColor px-1 md:px-2 md:py-1'>
             <FiChevronLeft size={18} />
           </button>
@@ -33,7 +33,7 @@ const Category = () => {
         </div>
       </div>
       <div id='carouselContent' className='CatCarousel flex items-center justify-start gap-4 md:gap-8 overflow-x-auto 
-      scroll-smooth mt-4'>
+      scroll-smooth mt-2 md:mt-4'>
         
         <div>
           <CategoryCart />
