e0f6b18f2f0be92c2a2474c8ebc9eb45aecbc348
Abdullah All Noman <MD Noman Hossain>
2023-06-10 16:37:24 +0600

    initials commit

--- components/Category/CategoryPageMobilePorsone.jsx
+++ components/Category/CategoryPageMobilePorsone.jsx
@@ -11,13 +11,14 @@ const CategoryPageMobilePorsone = () => {
     const [brand, setBrand] = useState(false)
     const [discount, setDiscount] = useState(false)
     const [price, setPrice] = useState(false)
+    const [customerRating, setCustomerRating] = useState(false)
 
     return (
         <div className='bg-white relative'>
             <div className='flex items-center gap-2 mt-1 p-2'>
                 <div className='short_bySection'>
-                    <span onClick={() => setShort(true)} className='flex items-center border border-bgColor gap-1 px-3 py-1 rounded-full'>
-                        <h1 className='xxsFont uppercase'>short by</h1>
+                    <span onClick={() => setShort(true)} className='flex items-center border border-bgColor gap-1 px-[10px] py-[5px] rounded-full w-fit'>
+                        <h1 className='xxsFont uppercase whitespace-nowrap'>short by</h1>
                         <HiOutlineChevronDown size={14} />
                     </span>
                     <div className={`fixed bottom-0 left-0 right-0  z-50 ${short ? `h-[100vh] ease-in-out transition-all duration-500` : `h-0 ease-in-out transition-all duration-500`} w-full  flex flex-col`}>
@@ -67,7 +68,7 @@ const CategoryPageMobilePorsone = () => {
                     </div>
                 </div>
                 <div className='brandSection'>
-                    <span onClick={() => setBrand(true)} className='flex items-center border border-bgColor gap-1 px-3 py-1 rounded-full'>
+                    <span onClick={() => setBrand(true)} className='flex items-center border border-bgColor gap-1 px-[10px] py-[5px] rounded-full'>
                         <h1 className='xxsFont uppercase'>brand</h1>
                         <HiOutlineChevronDown size={14} />
                     </span>
@@ -105,7 +106,7 @@ const CategoryPageMobilePorsone = () => {
                     </div>
                 </div>
                 <div className='discountSection'>
-                    <span onClick={() => setDiscount(true)} className='flex items-center border border-bgColor gap-1 px-3 py-1 rounded-full'>
+                    <span onClick={() => setDiscount(true)} className='flex items-center border border-bgColor gap-1 px-[10px] py-[5px] rounded-full'>
                         <h1 className='xxsFont uppercase'>discount</h1>
                         <HiOutlineChevronDown size={14} />
                     </span>
@@ -148,7 +149,8 @@ const CategoryPageMobilePorsone = () => {
                     </div>
                 </div>
                 <div className='priceSection'>
-                    <span onClick={() => setPrice(true)} className='flex items-center border border-bgColor gap-1 px-3 py-1 rounded-full'>
+                    <span onClick={() => setPrice(true)} className='flex items-center border border-bgColor gap-1 
+                    px-[10px] py-[5px] rounded-full'>
                         <h1 className='xxsFont uppercase'>price</h1>
                         <HiOutlineChevronDown size={14} />
                     </span>
@@ -175,6 +177,57 @@ const CategoryPageMobilePorsone = () => {
                                     <h4 className='text-xs font-medium capitalize'>tk 500 to tk 999</h4>
                                 </span>
 
+                            </div>
+                            <div className='bg-white flex items-center gap-5 mb-1 p-2'>
+                                <button className='px-2 py-1 bg-gray-500 text-white w-full'>Clear</button>
+                                <button className='px-2 py-1 bg-bgColor text-white w-full'>Apply</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='priceSection'>
+                    <span onClick={() => setCustomerRating(true)} className='flex items-center border border-bgColor gap-1 
+                    px-[10px] py-[5px] rounded-full w-fit'>
+                        <h1 className='xxsFont uppercase whitespace-nowrap'>customer rating</h1>
+                        <HiOutlineChevronDown size={14} />
+                    </span>
+                    <div className={`fixed bottom-0 left-0 right-0 z-50 ${customerRating ? `h-[100vh] ease-in-out transition-all duration-500` : `h-0 ease-in-out transition-all duration-500`} w-full  flex flex-col`}>
+                        <div onClick={() => setCustomerRating(false)} className='bg-black/70 h-full w-full'>
+
+                        </div>
+                        <div className='bg-white h-fit'>
+                            <h1 className='uppercase font-medium text-sm p-4'>
+                                filters
+                            </h1>
+                            <hr />
+                            <div className='p-4 mt-1 grid grid-cols-2 gap-3'>
+                                <span className='flex items-center gap-3 mb-1'>
+                                    <input name='short_by' value="" type="checkbox" className='checked w-3 h-3 accent-bgColor' />
+                                    <h4 className='text-xs font-medium  flex items-center '>
+                                        3 <picture>
+                                            <img className='h-3 mx-2' src="/SVG/star.svg" alt="" />
+                                        </picture>
+                                        above
+                                    </h4>
+                                </span>
+                                <span className='flex items-center gap-3 mb-1'>
+                                    <input name='short_by' value="" type="checkbox" className='checked w-3 h-3 accent-bgColor' />
+                                    <h4 className='text-xs font-medium  flex items-center '>
+                                        4 <picture>
+                                            <img className='h-3 mx-2' src="/SVG/star.svg" alt="" />
+                                        </picture>
+                                        above
+                                    </h4>
+                                </span>
+                                <span className='flex items-center gap-3 mb-1'>
+                                    <input name='short_by' value="" type="checkbox" className='checked w-3 h-3 accent-bgColor' />
+                                    <h4 className='text-xs font-medium  flex items-center '>
+                                        3.5 <picture>
+                                            <img className='h-3 mx-2' src="/SVG/star.svg" alt="" />
+                                        </picture>
+                                        above
+                                    </h4>
+                                </span>
                             </div>
                             <div className='bg-white flex items-center gap-5 mb-1 p-2'>
                                 <button className='px-2 py-1 bg-gray-500 text-white w-full'>Clear</button>
