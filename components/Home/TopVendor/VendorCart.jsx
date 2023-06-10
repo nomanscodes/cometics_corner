8991cf356907f8152fe44098625951c7f15cddf4
Abdullah All Noman <MD Noman Hossain>
2023-06-10 22:09:59 +0600

    initials commit

--- components/Home/TopVendor/VendorCart.jsx
+++ components/Home/TopVendor/VendorCart.jsx
@@ -5,7 +5,7 @@ import { MdOutlineSupervisorAccount } from "react-icons/md"
 const VendorCart = () => {
   return (
     <div className='w-auto bg-white  shadow'>
-      <div style={{ backgroundImage: `url("/happy.avif")` }} className="name fitImage h-28  rounded-sm">
+      <div style={{ backgroundImage: `url("/happy.avif")` }} className="name fitImage h-32  rounded-sm">
         <div className='p-2'>
           <h1 className='text-sm font-medium md:font-bold flex items-center gap-2'>Wellness Store <span><IoIosArrowForward size={13} /></span></h1>
           <h4 className='text-xs'>For all health needs</h4>
