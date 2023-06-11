d8b98c167fd780bd9b5237a65600de67aab49186
Abdullah All Noman <MD Noman Hossain>
2023-06-11 18:37:50 +0600

    initials commit

--- components/Home/Cetagory/CategoryCard.jsx
+++ components/Home/Cetagory/CategoryCard.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 
 const CategoryCart = ({ item }) => {
     return (
-        <Link href={"/category"} className='text-bgColor flex flex-col items-center gap-2 '>
+        <Link href={"/category"} className=' flex flex-col items-center gap-2 '>
 
             <div style={{ backgroundImage: `url(${item.image})` }} className='h-14  w-14  rounded-full flex items-center justify-center overflow-hidden fitImage'>
 
             </div>
-            <p className='text-xs md:text-sm font-semibold whitespace-nowrap'>{item.name}</p>
+            <p className='capitalize text-xs md:text-sm whitespace-nowrap'>{item.name}</p>
         </Link>
     )
 }
