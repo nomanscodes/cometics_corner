05e46c143ae6aab04ac0167b4b33c1a7c63ff7c0
Abdullah All Noman <MD Noman Hossain>
2023-06-16 15:04:41 +0600

    initials commit

--- components/ProductPage/MobileSection/MobileSection.jsx
+++ components/ProductPage/MobileSection/MobileSection.jsx
@@ -7,9 +7,22 @@ const MobileSection = () => {
     return (
         <div className='py-2 px-1'>
             <ImageSlider />
-            <hr className='mt-[1px] opacity-70'/>
+            <hr className='mt-[1px] opacity-80' />
             <div className='mt-2 px-2'>
-                <Link href={""} className='text-xs font-[400] text-bgColor'>View more from SEUNG</Link>
+                <div>
+                    <Link href={""} className='text-xs font-[400] text-green-800'>View more from SEUNG</Link>
+                    <h2 className='text-[13px] font-medium opacity-90 mt-1'>Basics Silky Smooth Body Wash, Peach & Orange Blossom Scent, 18 Fl Oz</h2>
+                    <span className='flex items-center gap-2 mt-2'>
+                        <picture className='flex items-center gap-1'>
+                            <img src="/SVG/star.svg" className='h-3' alt="" />
+                            <img src="/SVG/star.svg" className='h-3' alt="" />
+                            <img src="/SVG/star.svg" className='h-3' alt="" />
+                            <img src="/SVG/star.svg" className='h-3' alt="" />
+                        </picture>
+                        <h5 className='text-[11px] font-medium text-green-800'> 393 rating</h5>
+                    </span>
+                </div>
+
             </div>
         </div>
     )
