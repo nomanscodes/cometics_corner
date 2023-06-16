924f29a5de4205433137da2fcea5dc2b132f178f
Abdullah All Noman <MD Noman Hossain>
2023-06-16 14:56:10 +0600

    initials commit

--- components/ProductPage/MobileSection/MobileSection.jsx
+++ components/ProductPage/MobileSection/MobileSection.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import ImageSlider from './ImageSlider'
+import Link from 'next/link'
 
 
 const MobileSection = () => {
     return (
         <div className='py-2 px-1'>
             <ImageSlider />
+            <hr className='mt-[1px] opacity-70'/>
             <div className='mt-2 px-2'>
-                <h4 className='text-xs font-medium text-bgColor'>View more from SEUNG</h4>
+                <Link href={""} className='text-xs font-[400] text-bgColor'>View more from SEUNG</Link>
             </div>
         </div>
     )
