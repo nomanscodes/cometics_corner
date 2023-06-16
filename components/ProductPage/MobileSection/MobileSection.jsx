5d562627cb1a19ebc84b6e0c98d3ea502e7ab16b
Abdullah All Noman <MD Noman Hossain>
2023-06-16 23:31:34 +0600

    initials commit

--- components/ProductPage/MobileSection/MobileSection.jsx
+++ components/ProductPage/MobileSection/MobileSection.jsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 import { AiOutlineDislike, AiOutlineLike } from 'react-icons/ai'
 import { MdVerifiedUser } from 'react-icons/md'
 import ProductCart from '@/components/Home/Products/ProductCard'
-
+import { BiSearch } from 'react-icons/bi'
 
 const MobileSection = () => {
     return (
@@ -312,14 +312,56 @@ const MobileSection = () => {
                 </div>
             </div>
             <div className='question&answer mt-2 p-2 bg-white'>
-                <span className='flex items-center justify-between gap-12'>
+                <span className='flex items-center justify-between gap-12 relative'>
                     <h1 className='text-[13px] font-semibold opacity-95 mb-2 whitespace-nowrap'>QUESTIONS & ANSWERS</h1>
                     <input
                         type="text"
-                        className="w-full border rounded focus:outline-none text-xs py-[6px] pl-3"
+                        className="w-full border rounded focus:outline-none text-xs py-[6px] pl-7"
                         placeholder="Search here"
                     />
+                    <BiSearch size={15} className='absolute left-52 opacity-80' />
                 </span>
+                <div className='flex flex-col gap-3 mt-2'>
+                    <div>
+                        <h3 className='text-[12px] font-medium'>Q: It is shade 128</h3>
+                        <h4 className='text-[12px] mt-1'>A: 120</h4>
+                        <div className='flex items-center gap-5 mt-2'>
+                            <span className='flex items-center gap-1 border border-gray-400 px-2 py-1 rounded-full shadow'>
+                                <AiOutlineLike size={13} />
+                                <h5 className='text-[10px]'>Helpful for 43</h5>
+                            </span>
+                            <span className='flex items-center gap-1 border border-gray-400 px-2 py-1 rounded-full shadow'>
+                                <AiOutlineDislike size={13} />
+                                <h5 className='text-[10px]'>12</h5>
+                            </span>
+                        </div>
+                        <span className='flex items-center gap-1 mt-1'>
+                            <MdVerifiedUser size={12} />
+                            <h3 className='text-[11px] 
+                            font-medium'>Menom Seller</h3>
+                        </span>
+                    </div>
+                    <hr />
+                    <div>
+                        <h3 className='text-[12px] font-medium'>Q: Which brand is this?</h3>
+                        <h4 className='text-[12px] mt-1'>A: Blue Earth</h4>
+                        <div className='flex items-center gap-5 mt-2'>
+                            <span className='flex items-center gap-1 border border-gray-400 px-2 py-1 rounded-full shadow'>
+                                <AiOutlineLike size={13} />
+                                <h5 className='text-[10px]'>Helpful for 34</h5>
+                            </span>
+                            <span className='flex items-center gap-1 border border-gray-400 px-2 py-1 rounded-full shadow'>
+                                <AiOutlineDislike size={13} />
+                                <h5 className='text-[10px]'>6</h5>
+                            </span>
+                        </div>
+                        <span className='flex items-center gap-1 mt-1'>
+                            <MdVerifiedUser size={12} />
+                            <h3 className='text-[11px] 
+                            font-medium'>Menom Seller</h3>
+                        </span>
+                    </div>
+                </div>
             </div>
         </div>
     )
