6f637d71d83b21318e5bb31e30547c12c0a852c2
Abdullah All Noman <MD Noman Hossain>
2023-06-16 14:42:11 +0600

    initials commit

--- components/ProductPage/MobileSection/MobileSection.jsx
+++ components/ProductPage/MobileSection/MobileSection.jsx
@@ -4,8 +4,11 @@ import ImageSlider from './ImageSlider'
 
 const MobileSection = () => {
     return (
-        <div  className='py-2 px-1'>
+        <div className='py-2 px-1'>
             <ImageSlider />
+            <div className='mt-2 px-2'>
+                <h4 className='text-xs font-medium text-bgColor'>View more from SEUNG</h4>
+            </div>
         </div>
     )
 }
