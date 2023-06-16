f0b96471ae5e5bc2fcc0baa0016191184c24c6a9
Abdullah All Noman <MD Noman Hossain>
2023-06-16 17:04:42 +0600

    initials commit

--- components/ProductPage/MobileSection/MobileSection.jsx
+++ components/ProductPage/MobileSection/MobileSection.jsx
@@ -8,7 +8,7 @@ const MobileSection = () => {
         <div className='py-2'>
             <ImageSlider />
             <div className='mt-[3px] bg-white'>
-                <div className='px-2'>
+                <div className='p-2'>
                     <Link href={""} className='text-[11px] font-medium text-green-700'>View more from Blue Earth</Link>
                     <h2 className='text-[13px] font-medium opacity-90 mt-1'>Basics Silky Smooth Body Wash, Peach & Orange Blossom Scent, 18 Fl Oz</h2>
                     <span className='flex items-center gap-2 mt-1'>
@@ -27,30 +27,39 @@ const MobileSection = () => {
                     </span>
                 </div>
             </div>
-            <hr className='opacity-90' />
-            <div className='bg-white px-2'>
+            <div className='bg-white p-2'>
                 <table className=''>
                     <tbody>
-                        <tr className='mt-[2px]'>
-                            <td className='text-[11px] pr-5 opacity-90'>Payment</td>
+                        <tr className=''>
+                            
+                            <td className='text-[12px] pr-5 opacity-90'>Payment</td>
                             <td className='text-[11px] font-[500] '>Secure Transition</td>
                         </tr>
-                        <tr className='mt-[2px]'>
-                            <td className='text-[11px] pr-5 opacity-90 whitespace-nowrap'>
+                        <tr className=''>
+                            
+                            <td className='text-[12px] pr-5 opacity-90 whitespace-nowrap'>
                                 Ships From  </td>
                             <td className='text-[11px] font-[500]  '>Author Store</td>
                         </tr>
-                        <tr className='mt-[2px]'>
-                            <td className='text-[11px] pr-5 opacity-90'>Sold By</td>
+                        <tr className=''>
+                            
+                            <td className='text-[12px] pr-5 opacity-90'>Sold By</td>
                             <td className='text-[11px] font-[500]  '>Author</td>
                         </tr>
-                        <tr className='mt-[2px]'>
-                            <td className='text-[11px] pr-5 opacity-90 text-start'>Return  </td>
+                        <tr className=''>
+                            
+                            <td className='text-[12px] pr-5 opacity-90 text-start'>Return  </td>
                             <td className='text-[11px] font-[500] '>Eligible for return refund or replacement within 30 days of recepit</td>
+                        </tr>
+                        <tr className=''>
+                            
+                            <td className='text-[12px] pr-5 opacity-90 text-start whitespace-nowrap'>Cash on delivery</td>
+                            <td className='text-[11px] font-[500] '>Abailable</td>
                         </tr>
                     </tbody>
                 </table>
             </div>
+
         </div>
     )
 }
