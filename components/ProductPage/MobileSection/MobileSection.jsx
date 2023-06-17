1878f8a844efb9a5f5ee7e34edca78e6dcb622bf
Abdullah All Noman <MD Noman Hossain>
2023-06-17 12:29:40 +0600

    initials commit

--- components/ProductPage/MobileSection/MobileSection.jsx
+++ components/ProductPage/MobileSection/MobileSection.jsx
@@ -391,12 +391,12 @@ const MobileSection = () => {
                                 <AiOutlineSearch className=' absolute ml-1 mt-[4px] text-gray-500 border-solid border-neutral-500' size={20} />
                             </div>
                         </div>
-                        <div className='p-2'>
-                            <div className='flex flex-col'>
+                        <div className=''>
+                            <div className='p-2'>
                                 <h4 className='text-[11px] font-semibold'>Q : It is a sutable for oily skin?</h4>
                                 <h4 className='text-[11px] mt-[2px]'>A : Yes you can use this on all skin types</h4>
                                 <span className='flex items-center justify-between mt-1'>
-                                    <h4 className='text-[10px] mt-1 font-medium opacity-70'>Blue Earth</h4>
+                                    <h4 className='text-[11px] mt-[3px] font-medium opacity-70'>Blue Earth</h4>
                                     <span className='flex items-center gap-2'>
                                         <span className='flex items-center gap-1 opacity-80'>
                                             <AiOutlineLike className='' size={12} />
@@ -405,6 +405,29 @@ const MobileSection = () => {
                                         <span className='flex items-center gap-1 opacity-80'>
                                             <AiOutlineDislike className='' size={12} />
                                             <h4 className=' text-[11px] font-medium'>2</h4>
+                                        </span>
+                                    </span>
+                                </span>
+                                <span className='flex items-center gap-1 mt-[2px]'>
+                                    <FaUserShield size={12} className='opacity-75'/>
+                                    <h4 className='text-[11px]  font-medium opacity-70'> Menom User</h4>
+                                </span>
+                            </div>
+                            <hr />
+                            <div className='p-2'>
+                                <h4 className='text-[11px] font-semibold'>Q : Which brand is this?</h4>
+                                <h4 className='text-[11px] mt-[2px]'>A : This 
+                                brand is Uniliver</h4>
+                                <span className='flex items-center justify-between mt-1'>
+                                    <h4 className='text-[11px] mt-[3px] font-medium opacity-70'>Lorem Lots</h4>
+                                    <span className='flex items-center gap-2'>
+                                        <span className='flex items-center gap-1 opacity-80'>
+                                            <AiOutlineLike className='' size={12} />
+                                            <h4 className=' text-[11px] font-medium'>9</h4>
+                                        </span>
+                                        <span className='flex items-center gap-1 opacity-80'>
+                                            <AiOutlineDislike className='' size={12} />
+                                            <h4 className=' text-[11px] font-medium'>4</h4>
                                         </span>
                                     </span>
                                 </span>
