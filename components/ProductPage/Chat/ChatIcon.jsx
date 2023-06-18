493b475b4c0b3bd582cfc17ddd5bd5146fdcf305
Abdullah All Noman <MD Noman Hossain>
2023-06-18 21:06:36 +0600

    initials commit

--- components/ProductPage/Chat/ChatIcon.jsx
+++ components/ProductPage/Chat/ChatIcon.jsx
@@ -1,34 +1,96 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const ChatIcon = () => {
-
-    const [showNotify, setShowNotify] = useState(false)
-
-    const showMessage = (arg) => {
-
-        if (arg == "ok") {
-            setShowNotify(true)
-        }
-        if (arg == "bye") {
-            setTimeout(() => {
-                setShowNotify(false)
-            }, 1000);
-        }
-    }
+    const [showMessageBar, setshowMessageBar] = useState(false);
 
     return (
-        <div className='hidden md:block fixed bottom-6 right-8 z-50 '>
-            <picture>
-                <img src="/SVG/chat.svg"
-                    onMouseOver={() => showMessage("ok")}
-                    onMouseLeave={() => showMessage("bye")}
-                    className='h-12 cursor-pointer z-40' alt=""
-                />
-            </picture>
-            <h1 className={`text-white
-            bg-[#f54d66] rounded-sm shadow p-1 whitespace-nowrap text-[12px] font-medium uppercase absolute -mt-8 -ml-[129px] ${showNotify ? ` w-fit transition-all ease-in-out duration-500` : `ml-20 transition-all ease-in-out duration-500`} `}>Chat With Vendor</h1>
+        <div className="fixed bottom-14 right-1 md:bottom-4 md:right-6 z-50 ">
+            <div onClick={() => setshowMessageBar(!showMessageBar)} className="bg-[#085e54] p-[6px] md:p-2 h-8 w-8 md:h-12 md:w-12 rounded-full flex items-center justify-center  cursor-pointer z-40">
+                <picture>
+                    <img src="/SVG/chat.svg" className="h-7" alt="" />
+                </picture>
+            </div>
+            <div className={`h-[300px] md:h-[400px]  bg-[#e4e1d7] absolute bottom-[35px] md:bottom-[50px] rounded 
+            ${showMessageBar ? `w-[250px] md:w-96 right-0 ease-in-out transition-all duration-300`
+                    : `left-20 w-0 transition-all ease-in-out duration-300`} `}
+            >
+                <div className="messageTop flex items-center gap-3 bg-[#085e54] p-2 rounded">
+                    <picture>
+                        <img src="/avatar.png" className="h-9 w-9 rounded-full" alt="" />
+                    </picture>
+                    <span>
+                        <h1 className="vendorname text-sm font-medium text-white">
+                            Blue Earth
+                        </h1>
+                        <h3 className="text-[10px] text-white">
+                            Typically replies instantly
+                        </h3>
+                    </span>
+                </div>
+                <div className="messageBody p-[8px] md:p-3 flex flex-col gap-4 h-[200px] overflow-y-auto">
+                    <div className="reciveMessage flex items-end gap-2">
+                        <picture>
+                            <img
+                                src="/avatar.png"
+                                className=" h-5 w-5 md:h-7 md:w-7 rounded-full shadow"
+                                alt=""
+                            />
+                        </picture>
+                        <div className="bg-white w-fit max-w-[220px] md:max-w-[250px] rounded-lg rounded-bl-none shadow">
+                            <div className="text-[11px] font-medium p-[6px] md:p-2">
+                                jdlfsdl dhfsdf sdfdns shfffs fggfgfd jkfjdsfei fsd kfsdlkf
+                                fdfds fkjsdlslsdfsd dflsd
+                            </div>
+                            <div className="flex items-center justify-end text-[8px] px-2">
+                                <h3> 12:43 AM</h3>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="sendMessage flex items-center justify-end ">
+                        <div className="bg-white w-fit max-w-[209px] md:max-w-[250px] rounded-lg rounded-bl-none shadow">
+                            <div className="text-[11px] font-medium p-[6px] md:p-2">
+                                jdlfsdl dhfsdf sdfdns shfffs fggfgfd jkfjdsfei fsd kfsdlkf
+                                fdfds fkjsdlslsdfsd dflsd
+                            </div>
+                            <div className="flex items-center justify-end text-[8px] px-2">
+                                <h3> 12:43 AM</h3>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="reciveMessage flex items-end gap-2">
+                        <picture>
+                            <img
+                                src="/avatar.png"
+                                className=" h-5 w-5 md:h-7 md:w-7 rounded-full shadow"
+                                alt=""
+                            />
+                        </picture>
+                        <div className="bg-white w-fit max-w-[220px] md:max-w-[250px] rounded-lg rounded-bl-none shadow">
+                            <div className="text-[11px] font-medium p-[6px] md:p-2">
+                                jdlfsdl dhfsdf sdfdns shfffs fggfgfd jkfjdsfei fsd kfsdlkf
+                                fdfds fkjsdlslsdfsd dflsd
+                            </div>
+                            <div className="flex items-center justify-end text-[8px] px-2">
+                                <h3> 12:43 AM</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="radimateHrTag w-full bg-slate-400 h-[1px]"></div>
+                <div className="messageTypeSection flex items-center gap-2 p-2">
+                    <input
+                        type="text"
+                        className="flex flex-1 focus:outline-none text-xs pl-4
+                         py-[10px] bg-[#e4e1d7] "
+                        placeholder="Type your message"
+                    />
+                    <picture>
+                        <img src="/SVG/send.svg" className="h-5 cursor-pointer" alt="" />
+                    </picture>
+                </div>
+            </div>
         </div>
-    )
-}
+    );
+};
 
-export default ChatIcon
+export default ChatIcon;
