9350336623c82acce1c25147fe7e394546a18ee4
Abdullah All Noman <MD Noman Hossain>
2023-07-06 20:06:10 +0600

    customer order page create

--- components/Profile/ShippingAddres.jsx
+++ components/Profile/ShippingAddres.jsx
@@ -3,13 +3,15 @@ import AddressCard from '../Checkout.jsx/AddressCard'
 
 const ShippingAddress = () => {
   return (
-    <div className='md:flex items-center justify-center px-2 md:min-h-[60vh]'>
-
-      <AddressCard />
-      <AddressCard />
-      <AddressCard />
-
+    <div className='bg-white'>
+      <h1 className='text-[20px] mb-1 font-[500] flex items-center justify-center md:pt-4'>Shipping Address</h1>
+      <div className='md:flex items-center justify-center px-2 md:min-h-[60vh]'>
+        <AddressCard />
+        <AddressCard />
+        <AddressCard />
+      </div>
     </div>
+
   )
 }
 
